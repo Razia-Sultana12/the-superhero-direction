7455d9f848094d3465ed7ef09dbe4738beaa680e
Razia_Sultana12 <nisridhika@gmail.com>
2021-09-29 18:33:43 +0600

    last-commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,6 @@ const Cart = (props) => {
         <div className="cart">
             <h4>Favourite App List</h4>
             <h5>Selected Apps:{props.cart.length}</h5>
-            {/* <h5>Favourite App:{cart}</h5> */}
         </div>
 
        
